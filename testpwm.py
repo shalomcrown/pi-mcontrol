08a0a1c32f7a4a01ef546bcd8c849914d6850a9b
Shalom Crown <shalomcrown@gmail.com>
2015-08-12 18:59:38 +0300

    Started changing to use pigpio

--- testpwm.py
+++ testpwm.py
@@ -5,51 +5,97 @@
 #
 ############################################################
 
+import os
+
 import RPi.GPIO as GPIO
-from bottle import route, run
+import pigpio
+from bottle import route, run, static_file
 
-# Speeds in m/s
-groundSpeedRequested = 0.0
-groudSpeedActual = 0.0
 
-#Angular speeds in radians/s
-yawSpeedRequested = 0.0
-yawSpeedActual = 0.0
 
-leftPwmPercent = 0.0
-rightPwmPercent = 0.0
 
-# v = K p
-# - Simple linear relationship....
-# p is precentage PWM
-# This is the K value - assume top speed is 0.5 m/s
-speedToPwmPercent = 0.5 / 100
+class motorControlBase:
+    def __init__(self):
+        # Speeds in m/s
+        self.groundSpeedRequested = 0.0
+        self.groudSpeedActual = 0.0
+
+        #Angular speeds in radians/s
+        self.yawSpeedRequested = 0.0
+        self.yawSpeedActual = 0.0
+
+        self.leftPwmPercent = 0.0
+        self.rightPwmPercent = 0.0
+
+        # v = K p
+        # - Simple linear relationship....
+        # p is precentage PWM
+        # This is the K value - assume top speed is 0.5 m/s
+        self.speedToPwmPercent = 0.5 / 100
+        self.setupPwm()
+
+    def stopPwm(self):
+        self.setLeftPwm(0)
+        self.setRightPwm(0)
+
+    def faster(self, steps = 0.1):
+        if self.leftPwmPercent < 1 and self.rightPwmPercent < 1:
+            self.groundSpeedRequested = self.groundSpeedRequested + steps
+            self.leftPwmPercent = self.rightPwmPercent = self.groundSpeedRequested / self.speedToPwmPercent
+            self.setLeftPwm(self.leftPwmPercent)
+            self.setRightPwm(self.rightPwmPercent)
+            return self.groundSpeedRequested
+        else:
+            raise Exception("Already at speed limit")
 
 #==================================================================
 
-def setupPwm():
-    GPIO.setmode(GPIO.BOARD)
-    GPIO.setup(32, GPIO.OUT) # Pin 32 on header (GPIO 12) is PWM0
-    GPIO.setup(35, GPIO.OUT) # Pin 35 in header - GPIO 19 is PWM1
-    global leftPwm = GPIO.PWM(32, 5000)
-    global rightPwm = GPIO.PWM(35, 5000)
-    leftPwm.start(0)
-    rightPwm.start(0)
-    
-def teardownPwm()
-    leftPwm.stop()
-    rightPwm.stop()
-    GPIO.cleanup()
+class motorControlGpio(motorControlBase):
+    def __init__(self):
+        motorControlBase.__init__()
 
-def setLeftPwm(value):
-    leftPwm.ChangeDutyCycle(value)
+    def setupPwm(self):
+        GPIO.setmode(GPIO.BOARD)
+        GPIO.setup(32, GPIO.OUT) # Pin 32 on header (GPIO 12) is PWM0
+        GPIO.setup(35, GPIO.OUT) # Pin 35 in header - GPIO 19 is PWM1
+        self.leftPwm = GPIO.PWM(32, 5000)
+        self.rightPwm = GPIO.PWM(35, 5000)
+        self.leftPwm.start(0)
+        self.rightPwm.start(0)
 
-def setRightPwm(value):
-    rightPwm.ChangeDutyCycle(value)
+    def teardownPwm(self):
+        self.leftPwm.stop()
+        self.rightPwm.stop()
+        GPIO.cleanup()
 
-def stopPwm():
-    setLeftPwm(0)
-    setRightPwm(0)
+    def setLeftPwm(self, value):
+        self.leftPwm.ChangeDutyCycle(value)
+
+    def setRightPwm(self, value):
+        self.rightPwm.ChangeDutyCycle(value)
+
+#==================================================================
+class motorControlPig(motorControlBase):
+    def __init__(self):
+        super(motorControlPig, self).__init__()
+        self.pig = pigpio.pi()
+
+    def teardownPwm(self):
+        setLeftPwm(0)
+        setRightPwm(0)
+        self.pig.stop()
+
+    def setupPwm(self):
+        setLeftPwm(0)
+        setRightPwm(0)
+
+    def setLeftPwm(self, value):
+        value = value * 255 / 100
+        self.pig.set_PWM_dutycycle(32, value)
+
+    def setRightPwm(self, value):
+        value = value * 255 / 100
+        self.pig.set_PWM_dutycycle(35, value)
 
 
 #==================================================================
@@ -58,37 +104,29 @@ def stopPwm():
 # Add 0.1 m/s - up to maximum
 @route("/faster")
 def faster():
-    if leftPwmPercent < 1 and rightPwmPercent < 1:
-        groundSpeedRequested = groundSpeedRequested + 0.1
-        leftPwmPercent = rightPwmPercent = groundSpeedRequested / speedToPwmPercent
-        setLeftPwm(leftPwmPercent)
-        setRightPwm(rightPwmPercent)
-        return groundSpeedRequested
-    else:
+    try:
+        ctl.faster()
+    except:
         return  { "success" : False, "error" : "Already at top speed" }
 
 @route("/stop")
 def stop():
-    stopPwm()
+    ctl.stopPwm()
     return "OK"
-    
 
-
-#=================================================================
-
-def testPwm():
-    setupPwm()
-
+@route("/")
+def baseFile():
+    localpath = os.path.realpath(__file__)
+    return static_file('index.html', root=localpath, download='index.html')
 
 
 
-    input('Press any key to stop:')
-    teardownPwm()
 
 
 
 #=================================================================
 if __name__ == "__main__":
+    ctl = motorControlPig()
 
-
-    run(host='localhost', port=8080)
+    run(host="0.0.0.0", port=8080)
+    ctl.teardownPwm()
